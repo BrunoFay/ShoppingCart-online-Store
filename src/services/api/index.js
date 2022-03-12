05c448edf912b720044d915c1948b904b5beedd1
Bruno Alves Fay <brunofay1@hotmail.com>
2022-03-11 23:50:02 -0300

    change api request method

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,13 +1,17 @@
 export async function getCategories() {
  
   const answer = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((response) => response.json());
-  return answer;
+    const data = await answer.json()
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  
+ try {
   const answer = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-    .then((response) => response.json());
-  return answer;
+  const data = await answer.json()
+  return data;
+ } catch (error) {
+   return console.log(error);
+ }
+ 
 }
